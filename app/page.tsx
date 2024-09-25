04fb18e1a48a634242b947afe381d0fe95687709
abhishek-shivale <abhishekshivale45@gmail.com>
2024-09-25 20:08:56 +0530

    [FEAT]: Added isLoggedIn featur

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { signOut, useSession } from 'next-auth/react';
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
