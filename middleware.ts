0393ad01e07baba3e7ce9313fdfcc03c0e5a8523
abhishek-shivale <abhishekshivale45@gmail.com>
2024-11-30 18:28:51 +0530

    solved token issue on production

--- middleware.ts
+++ middleware.ts
@@ -4,32 +4,22 @@ import { NextRequest, NextResponse } from 'next/server';
 const publicPaths = ['/login', '/register'];
 
 export async function middleware(req: NextRequest) {
-  try {
-    const token = await getToken({
-      req,
-      secret: process.env.NEXTAUTH_SECRET,
-    });
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
 
-    const path = req.nextUrl.pathname;
+  const path = req.nextUrl.pathname;
 
-    console.log('Request Headers:', req.headers);
-    console.log('Cookies:', req.cookies);
-    console.log('NEXTAUTH_SECRET:', process.env.NEXTAUTH_SECRET);
-
-    console.log('Token:1', token);
-
-    if (publicPaths.includes(path) && token) {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-
-    if (path.startsWith('/dashboard') && !token) {
-      return NextResponse.redirect(new URL('/login', req.url));
-    }
-
-    return NextResponse.next();
-  } catch (e) {
-    console.log("Error:", e);
+  if (publicPaths.includes(path) && token) {
+    return NextResponse.redirect(new URL('/', req.url));
   }
+
+  if (path.startsWith('/dashboard') && !token) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
