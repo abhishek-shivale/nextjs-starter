2396480b2b1996fbf45908349b1b1b28da4ba12d
abhishek-shivale <abhishekshivale45@gmail.com>
2024-11-30 18:18:12 +0530

    error

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,37 @@
 import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
-const publicPaths = ['/login', '/register',];
+const publicPaths = ['/login', '/register'];
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+  try {
+    const token = await getToken({
+      req,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
 
-  const path = req.nextUrl.pathname;
+    const path = req.nextUrl.pathname;
 
-  console.log("Request Headers:", req.headers);
-  console.log("Cookies:", req.cookies);
-  console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
+    console.log('Request Headers:', req.headers);
+    console.log('Cookies:', req.cookies);
+    console.log('NEXTAUTH_SECRET:', process.env.NEXTAUTH_SECRET);
 
-  console.log("Token:", token);
+    console.log('Token:1', token);
 
+    if (publicPaths.includes(path) && token) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
 
-  if (publicPaths.includes(path) && token) {
-    return NextResponse.redirect(new URL('/', req.url));
+    if (path.startsWith('/dashboard') && !token) {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
+
+    return NextResponse.next();
+  } catch (e) {
+    console.log("Error:", e);
   }
-
-  if (path.startsWith('/dashboard') && !token) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
-
-  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
